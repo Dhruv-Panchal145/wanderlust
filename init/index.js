cd997b6074e54b3e859d9ddf8ecbbe42dd830858
Dhruv Panchal <dhruvpanchal1345@gmail.com>
2026-04-21 10:45:10 +0530

    fix  in app.ejs

--- init/index.js
+++ init/index.js
@@ -1,26 +1,26 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
+const MONGO_URL = process.env.ATLASTDB;
+
 main()
 .then(() => {
     console.log("connected to DB")
 }).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL); // ✅ ATLASTDB → MONGO_URL
 }
-
 
 const initDB = async () => {
    await Listing.deleteMany({});
-
    const data = initData.map((obj) => ({
       ...obj,
       owner: "69d77c5b9878157cd5a1ac92"
    }));
-
    await Listing.insertMany(data);
    console.log("data was initialized");
 }
